665df8cfa40e1abd450a403947622ffd1da93367
Pasha127 <pasha.account@gmail.com>
2022-07-06 16:23:01 +0200

    part 2 in progress

--- Objects, Arrays Loops.js
+++ Objects, Arrays Loops.js
@@ -6,25 +6,63 @@
  [1, 3, 5] ==> [5, 3, 1]
 */
 
-/* WRITE YOUR ANSWER HERE */
+let extra01 = [1, 3, 5];
+console.log("before reversal:",extra01)
+extra01 = extra01.reverse();
+console.log("after reversal:", extra01);
 
 /* EXTRA 2
  Write a piece of code for getting the maximum numerical value from an array.
 */
 
-/* WRITE YOUR ANSWER HERE */
+let testArray =[0,61,2,35,4,45,63,7,28,9];
+console.log("Our starting array is:", testArray);
+let biggerNumber = null;
+let maxNumber = testArray[0];
+for(i=0;i<testArray.length;i++){
+    if(testArray[i+1]>testArray[i]){
+        biggerNumber=testArray[i+1]
+        //console.log("debug - higher");
+        if(biggerNumber>maxNumber){
+            maxNumber = biggerNumber;
+            //console.log("debug - new max");
+        }
+        
+    }//else{
+        //console.log("debug - lower");
+    //}
+}
+console.log("The max number is:", maxNumber);
+
 
 /* EXTRA 3
  Write a piece of code for getting the minimum numerical value from an array.
 */
 
-/* WRITE YOUR ANSWER HERE */
+let testArray02 =[1,61,2,35,4,0,63,7,28,9];
+console.log("Our starting array is:", testArray02);
+let smallerNumber = null;
+let minNumber = testArray02[0];
+for(i=0;i<testArray02.length;i++){
+    if(testArray02[i+1]<testArray02[i]){
+        smallerNumber=testArray02[i+1]
+        //console.log("debug - lower");
+        if(smallerNumber<minNumber){
+            minNumber = smallerNumber;
+            //console.log("debug - new min");
+        }
+        
+    }//else{
+        //console.log("debug - higher");
+    //}
+}
+console.log("The min number is:", minNumber);
 
 /* EXTRA 4
  Write a piece of code for getting only even numerical values from an array.
 */
 
-/* WRITE YOUR ANSWER HERE */
+console.log("Our starting array is:", testArray02);
 
 /* EXTRA 5
  Write a piece of code for deleting only even entries from an array.
